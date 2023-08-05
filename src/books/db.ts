80bb2048a248b4b6580b8f45e4f671e79585de38
rahulgarg55 <gargr0109@gmail.com>
2023-08-05 18:42:09 +0530

    commit

--- src/books/db.ts
+++ src/books/db.ts
@@ -1,8 +1,5 @@
 import pgPromise from 'pg-promise';
-
 const pgp = pgPromise();
-
 const connectionString = 'postgres://username:1234@localhost:5432/books-crud';
 const db = pgp(connectionString);
-
 export default db;
